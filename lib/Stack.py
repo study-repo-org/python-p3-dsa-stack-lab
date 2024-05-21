b96762c1f355158c410a3ac04fe512cf3d11d894
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-21 09:25:18 +0300

    Stack Lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,39 @@
 class Stack:
-
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=100):
+        if items is None:
+            items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+            return True 
+        else:
+            return False 
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None  
 
     def peek(self):
-        pass
-    
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None  
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        for index, item in enumerate(self.items[::-1]):
+            if item == target:
+                return index
+        return -1
